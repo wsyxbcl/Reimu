56b3b196a8786636156be734b6f5f9e12e7f9bbc
wsyxbcl <wsyxbcl@gmail.com>
2021-02-16 19:53:31 +0800

    update to split-adjusted price API, #6

--- utils.py
+++ utils.py
@@ -19,8 +19,9 @@ matplotlib.rcParams['font.family'] = ['Source Han Sans']
 
 data_path = './data'
 _test_path = './demo'
-#TODO API for split-adjusted share prices
-eastmoney_base = "http://push2his.eastmoney.com/api/qt/stock/kline/get?secid={market}.{bench_code}&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58&klt=101&fqt=0&beg={time_begin}&end={time_end}"
+# fqt is for split-adjusted price
+# eastmoney_base = "http://push2his.eastmoney.com/api/qt/stock/kline/get?secid={market}.{bench_code}&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58&klt=101&fqt=0&beg={time_begin}&end={time_end}"
+eastmoney_base = "http://push2his.eastmoney.com/api/qt/stock/kline/get?secid={market}.{bench_code}&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58&klt=101&fqt=1&beg={time_begin}&end={time_end}"
 
 
 #TODO still need a check
