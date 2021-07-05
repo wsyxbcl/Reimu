3731766ff5c059cf36839b19fa2d9423dcc28281
wsyxbcl <wsyxbcl@gmail.com>
2021-07-05 18:25:32 +0800

    seperate file operation from list comprehension...

--- bot.py
+++ bot.py
@@ -184,8 +184,12 @@ async def status(message):
         await message.reply(argparse_status.__doc__)
         return 0
     if args.query:
-        results = [pickle.load(f) for f in os.listdir(data_path) if args.stock_mix_code in f]
-        await message.reply('\n'.join([portfolio.__repr__ for portfolio in results]))
+        results = [f for f in os.listdir(data_path) if args.stock_mix_code in f]
+        portfolios = []
+        for f in results:
+            with open(os.path.join(data_path, f), 'rb') as fp:
+                portfolios.append(pickle.load(fp))
+        await message.reply('\n'.join([portfolio.__repr__ for portfolio in portfolios]))
         return 0
     stock_list = stock_query(keyword=args.stock_mix_code)
     logging.info(f'query result:{stock_list}')
