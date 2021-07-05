35b4c5419dc1876731d50139a6931e84222fbff1
wsyxbcl <wsyxbcl@gmail.com>
2021-07-05 18:13:22 +0800

    implement a query for stock_mix

--- bot.py
+++ bot.py
@@ -183,6 +183,10 @@ async def status(message):
     if args.help:
         await message.reply(argparse_status.__doc__)
         return 0
+    if args.query:
+        results = [pickle.load(f) for f in os.listdir(data_path) if args.stock_mix_code in f]
+        await message.reply('\n'.join([portfolio.__repr__ for portfolio in results]))
+        return 0
     stock_list = stock_query(keyword=args.stock_mix_code)
     logging.info(f'query result:{stock_list}')
     if len(stock_list) == 1 and type(stock_mix := stock_list[0]) is Stock_mix:
